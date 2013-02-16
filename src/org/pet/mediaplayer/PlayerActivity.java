a3c9d30ca4977f5ff5dd82c249ce57a4bc3e8ea2
Mohd Khairul Ikhwan Kamarudin <petalyaa@gmail.com>
2013-02-16 10:46:24 +0800

    Update UI, add repeat, shuffle, etc

--- src/org/pet/mediaplayer/PlayerActivity.java
+++ src/org/pet/mediaplayer/PlayerActivity.java
@@ -1,16 +1,18 @@
 package org.pet.mediaplayer;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.pet.mediaplayer.BasePlayer.PlayerState;
 import org.pet.mediaplayer.listener.OnAudioSeekBarChangeListener;
-import org.pet.mediaplayer.listener.OnPauseButtonClickListener;
-import org.pet.mediaplayer.listener.OnPlayButtonClickListener;
+import org.pet.mediaplayer.listener.OnMuteButtonClickListener;
+import org.pet.mediaplayer.listener.OnPlayPauseButtonClickListener;
+import org.pet.mediaplayer.listener.OnRepeatButtonClickListener;
+import org.pet.mediaplayer.listener.OnShuffleButtonClickListener;
 import org.pet.mediaplayer.listener.OnStopButtonClickListener;
-import org.pet.mediaplayer.mp3.MP3File;
 import org.pet.mediaplayer.mp3.MP3Player;
 import org.pet.mediaplayer.util.SystemUiHider;
-import org.pet.mediaplayer.util.TimeUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -64,59 +66,63 @@ public class PlayerActivity extends Activity {
 		Log.d(TAG, "Creating application...");
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.activity_player);
+        setContentView(R.layout.activity_player);
 		
 		// TODO : Change below chunk to read file from media server. Hardcoded just for POC!
+        List<AudioFile> audioFiles = new ArrayList<AudioFile>();
 		File sdCard= Environment.getExternalStorageDirectory();
-		File mp3File = new File(sdCard, "/Music/Mezmerize/BYOB.mp3");
+		File byob = new File(sdCard, "/Music/Mezmerize/BYOB.mp3");
+		File cigaro = new File(sdCard, "/Music/Mezmerize/Cigaro.mp3");
+		File question = new File(sdCard, "/Music/Mezmerize/Question.mp3");
+		File revenga = new File(sdCard, "/Music/Mezmerize/Revenga.mp3");
+		audioFiles.add(new AudioFile(byob));
+		audioFiles.add(new AudioFile(cigaro));
+		audioFiles.add(new AudioFile(question));
+		audioFiles.add(new AudioFile(revenga));
+		
 		// Getting file list done.
 		
 		TextView songTitle = (TextView) findViewById(R.id.song_title);
 		TextView artistName = (TextView) findViewById(R.id.artist_name);
 		TextView albumName = (TextView) findViewById(R.id.album_name);
 		TextView endTime = (TextView) findViewById(R.id.end_time);
+		TextView shuffleLabel = (TextView) findViewById(R.id.shuffle_label);
+		TextView repeatLabel = (TextView) findViewById(R.id.repeat_label);
 		final TextView startTime = (TextView) findViewById(R.id.start_time);
 		songSeekBar = (SeekBar) findViewById(R.id.song_seekbar);
 		
-		ImageButton playButton = (ImageButton) findViewById(R.id.play_button);
 		ImageButton stopButton = (ImageButton) findViewById(R.id.stop_button);
-		ImageButton pauseButton = (ImageButton) findViewById(R.id.pause_button);
+		ImageButton playPauseButton = (ImageButton) findViewById(R.id.pause_button);
+		ImageButton muteButton = (ImageButton) findViewById(R.id.mute_button);
+		ImageButton shuffleButton = (ImageButton) findViewById(R.id.shuffle_button);
+		ImageButton repeatButton = (ImageButton) findViewById(R.id.repeat_button);
 		
 		// TODO : For now, test using mp3, next check file type, then use appropriate player to play the audio.
-		MP3File mp3 = new MP3File(mp3File);
+		InfoPanelView infoPanel = new InfoPanelView(albumName, songTitle, artistName, endTime);
 		if(player == null) {
-			player = new MP3Player(getApplicationContext(), mp3, songSeekBar);
+			player = new MP3Player(getApplicationContext(), audioFiles, songSeekBar, infoPanel, playPauseButton);
+			player.setState(PlayerState.STOP);
 		} 
 		player.resetSeekBar(songSeekBar);
-		byte[] albumArt = mp3.getAlbumArt();
-		if(albumArt != null) {
-			Log.v(TAG, "Album art exist.");
-		}
-		
-		long originalDuration = mp3.getDuration();
-		long duration = originalDuration / 1000; // in seconds
-		
-		String endTimeStr = TimeUtil.secondsToString((int) duration);
-		
-		albumName.setText(mp3.getAlbumName());
-		songTitle.setText(mp3.getSongTitle());
-		artistName.setText(mp3.getArtistName());
-		endTime.setText(endTimeStr);
+		player.resetInfoPanel(infoPanel);
+		player.resetPlayButton(playPauseButton);
 		
 		OnSeekBarChangeListener seekBarChangeListener = new OnAudioSeekBarChangeListener(startTime, player, getApplicationContext());
-		
-		songSeekBar.setMax((int) originalDuration);
 		songSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
 		
 		// Setup all button
-		OnClickListener onPlayButtonClickListener = new OnPlayButtonClickListener(player, getApplicationContext());
-		OnClickListener onStopButtonClickListener = new OnStopButtonClickListener(player, getApplicationContext());
-		OnClickListener onPauseButtonClickListener = new OnPauseButtonClickListener(player, getApplicationContext());
+		OnClickListener onShuffleButtonClickListener = new OnShuffleButtonClickListener(player, getApplicationContext(), shuffleLabel);
+		OnClickListener onRepeatButtonClickListener = new OnRepeatButtonClickListener(player, getApplicationContext(), repeatLabel);
+		OnClickListener onMuteButtonClickListener = new OnMuteButtonClickListener(player, getApplicationContext(), muteButton);
+		OnClickListener onStopButtonClickListener = new OnStopButtonClickListener(player, getApplicationContext(), playPauseButton);
+		OnClickListener onPlayPauseButtonClickListener = new OnPlayPauseButtonClickListener(player, getApplicationContext(), playPauseButton);
 		
-		playButton.setOnClickListener(onPlayButtonClickListener);
+		shuffleButton.setOnClickListener(onShuffleButtonClickListener);
+		repeatButton.setOnClickListener(onRepeatButtonClickListener);
+		muteButton.setOnClickListener(onMuteButtonClickListener);
+		playPauseButton.setOnClickListener(onPlayPauseButtonClickListener);
 		stopButton.setOnClickListener(onStopButtonClickListener);
-		pauseButton.setOnClickListener(onPauseButtonClickListener);
 		
 	}
-
+	
 }
